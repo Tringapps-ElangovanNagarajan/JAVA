8d018e068f465f9e7dd18bd868d2e1483782f188
Tringapps - ElangovanNagarajan <96721489+Tringapps-ElangovanNagarajan@users.noreply.github.com>
2022-04-06 11:05:00 +0530

    Add files via upload

---
+++ Data Structure/QueueList.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+class Queue {
+	class Node{
+		int data;
+		Node next;
+		
+		Node(int val){
+			data = val;
+			next = null;
+		}
+	}
+	
+	Node front,rear; 
+	
+	Queue(){
+		front = null;
+		rear = null;
+	}
+	public void enqueue(int val) { 
+		Node newNode = new Node(val);
+		if(front==null)
+			front = newNode;
+		else
+			rear.next = newNode	;
+		rear = newNode;
+		
+	}
+	
+	public int dequeue() {
+		if(front==null)
+			throw new IndexOutOfBoundsException("Queue is Empty");
+		int temp = front.data;
+		front = front.next;
+		if(front==null) 
+			rear=null;
+		return temp;
+		
+	}
+}
+public class QueueList {
+	public static void main(String args[]) {
+		Queue li=new Queue();
+		Scanner sc=new Scanner(System.in);
+		while(true) {
+		System.out.println("1.Enqueue");
+		System.out.println("2.Deque");
+		int choice=sc.nextInt();
+		switch(choice) {
+		case 1:System.out.print("Enter your data = ");
+		   int val=sc.nextInt();
+		   li.enqueue(val);
+		   break;
+		case 2:System.out.println(li.dequeue() + " dequeue from Queue");
+		       break;
+		default:
+			System.out.println("You entered the Wrong Choice!Try again");
+		}
+		}
+}
+}
